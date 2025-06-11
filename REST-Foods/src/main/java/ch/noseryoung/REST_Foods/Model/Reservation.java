2060b1d7ff7f73e644800031d4e9d8d84815ae06
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-06-11 15:00:03 +0200

    24-Model_Update
    A file was added which has all the needed attributes for the table reservation and also the requirement like NotNull.

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/Model/Reservation.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/Model/Reservation.java
@@ -1,4 +1,41 @@
 package ch.noseryoung.REST_Foods.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Timer;
+import java.util.UUID;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="reservation")
+
 public class Reservation {
-}
+
+        @Id
+        @GeneratedValue
+        @Column(name = "reservation_id",  nullable = false)
+        private UUID id;
+
+        @NotEmpty
+        private LocalTime time;
+
+        @NotEmpty
+        private Date dateTime;
+
+        @NotEmpty
+        private Integer partySize;
+
+        @NotEmpty
+        private String name;
+        
+    }
+
