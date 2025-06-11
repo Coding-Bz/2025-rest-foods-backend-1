c4355816eac6afd0b5b34c8ebf39d1bbc91810ea
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-06-11 16:02:39 +0200

    add ReservationService

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
@@ -1,4 +1,22 @@
 package ch.noseryoung.REST_Foods.domain.Controller;
 
+import ch.noseryoung.REST_Foods.domain.Model.Reservation;
+import ch.noseryoung.REST_Foods.domain.Service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
 public class ReservationController {
+
+    @Autowired
+    ReservationService reservationService;
+
+
+
 }
