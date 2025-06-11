9343ea9cf1ee2f550eb510bedd8702c255040667
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-06-11 14:46:30 +0200

    24-Model_Update
    A file was added which has all the needed attributes for the table menu and also the requirement like NotNull. Besides from that the next step would be to do the same thing for reservations. NOTE: Adding it again because of the folder structure change

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/Model/Menu.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/Model/Menu.java
@@ -1,4 +1,32 @@
 package ch.noseryoung.REST_Foods.Model;
 
+import jakarta.persistence.*;
+        import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+        import java.util.UUID;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="menu")
 public class Menu {
+    @Id
+    @GeneratedValue
+    @Column(name = "menu_id",  nullable = false)
+    private UUID id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private Integer price;
+    @NotEmpty
+    private String category;
+    @NotEmpty
+    @Column(name = "dietary_requirements")
+    private String dietaryReq;
+    private String allergies;
+    private String description;
+    @NotEmpty
+    @Column(name = "chefs_Choice")
+    private Boolean chefsChoice;
 }
