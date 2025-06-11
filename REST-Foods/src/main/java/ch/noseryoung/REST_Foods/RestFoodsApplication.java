722bb8eb12ce71184d65b70461ae3c455935a9c0
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-06-11 11:04:52 +0200

    Spring Boot Project added

---
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/RestFoodsApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.REST_Foods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFoodsApplication.class, args);
+	}
+
+}
---
+++ REST-Foods/src/test/java/ch/noseryoung/REST_Foods/RestFoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.REST_Foods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
