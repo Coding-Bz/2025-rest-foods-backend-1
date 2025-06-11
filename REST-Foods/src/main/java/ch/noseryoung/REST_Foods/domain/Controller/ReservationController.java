ece607b52098f4f792a2ffd6d223b0ec18c7a204
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-06-11 17:01:21 +0200

    add test data

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/accounts")
+@RequestMapping("/api/v1")
 public class ReservationController {
 
     @Autowired
