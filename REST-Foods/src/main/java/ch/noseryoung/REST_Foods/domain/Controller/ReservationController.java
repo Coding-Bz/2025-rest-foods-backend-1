5f07cdadb809d05ad38e2302f4aef8f5fa585778
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-06-12 09:23:41 +0200

    change api

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/ReservationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/reservations")
+@RequestMapping("/api/v1/reservation")
 public class ReservationController {
 
     @Autowired
