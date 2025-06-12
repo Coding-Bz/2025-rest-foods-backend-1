3c9391092ed6f104d8314bbc79c48f9fdc7518d7
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-06-12 08:59:10 +0200

    24-Update_MenuController
    The Controller part of menu is done and they are no errors and for a stable code I also added exceptions. All the CRUD operations can be used. Service and Controller are full done unless they are going to be some changes from reservation part which could affect the menu parts.

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/MenuController.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/MenuController.java
@@ -1,7 +1,52 @@
 package ch.noseryoung.REST_Foods.domain.Controller;
 
+import ch.noseryoung.REST_Foods.domain.Model.Menu;
+import ch.noseryoung.REST_Foods.domain.Services.MenuService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
+@RestController
+@RequestMapping("/api/v1/")
 public class MenuController {
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService customerService) {
+        this.menuService = customerService;
+    }
+
+    @GetMapping("/menu")
+    public ResponseEntity<List<Menu>> getAllMenus() {
+        return new ResponseEntity<>(menuService.getAllMenus(), HttpStatus.OK);
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<Menu> getMenuById(@Valid @PathVariable UUID customerId) {
+        return new ResponseEntity<>(menuService.getMenuByID(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<Menu> createMenu(@Valid @RequestBody Menu customer) {
+        return new ResponseEntity<>(menuService.createMenu(customer), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<Menu> updateMenu(@Valid @PathVariable UUID id, @RequestBody Menu details) throws Exception {
+        return new ResponseEntity<>(menuService.updateMenu(id, details), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/customer/{customerId}")
+    public ResponseEntity<Void> deleteMenu(@Valid @PathVariable("customerId") UUID customerId) {
+        menuService.deleteMenu(customerId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
