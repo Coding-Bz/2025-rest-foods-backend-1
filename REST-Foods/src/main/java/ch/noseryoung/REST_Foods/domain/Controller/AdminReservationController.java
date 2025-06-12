b87f0c66f99d72177c9297d68961b4e03dd09420
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-06-12 16:13:33 +0200

    24-UpdateReservation
    Now the user is only able to update reservation if tehre is more than 24h time gap. I deleted the Endpoint to delete a reservation because only Admin can do that

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/AdminReservationController.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Controller/AdminReservationController.java
@@ -4,6 +4,7 @@ import ch.noseryoung.REST_Foods.domain.Model.Menu;
 import ch.noseryoung.REST_Foods.domain.Model.Reservation;
 import ch.noseryoung.REST_Foods.domain.Service.AdminReservationService;
 import ch.noseryoung.REST_Foods.domain.Service.ReservationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/admin/reservation")
@@ -82,8 +84,15 @@ public class AdminReservationController {
         return count;
 
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable UUID id) {
+        adminReservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
 
-
-
+    @PutMapping("/{id}")
+    public ResponseEntity<Reservation> updateReservation(@PathVariable UUID id, @Valid @RequestBody Reservation reservation) {
+        return ResponseEntity.ok(adminReservationService.updateReservation(id, reservation));
+    }
 
 }
--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Service/ReservationService.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Service/ReservationService.java
@@ -6,6 +6,8 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,12 +31,18 @@ public class ReservationService {
 
     public Reservation updateReservation(UUID id, Reservation reservation) {
         reservationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("This reservation doesn't exists"));
+
+        LocalDateTime combined = LocalDateTime.of(reservation.getDate(), reservation.getTime());
+
+        if (combined.isBefore(LocalDateTime.now().plusHours(24))) {
+            throw new IllegalArgumentException("Reservation cannot be updated by emergencies plaese contact Admin.");
+        }
+
         reservation.setId(id);
         return reservationRepository.save(reservation);
     }
 
-    public void deleteReservation(UUID id) {
-        reservationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("This reservation doesn't exists"));
-        reservationRepository.deleteById(id);
-    }
+
+
+
 }
