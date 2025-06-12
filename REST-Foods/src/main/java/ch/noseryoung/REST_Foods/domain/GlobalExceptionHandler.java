ed66ffdfed79512feee2b3c3c8627d8f15d5ee50
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-06-12 14:15:41 +0200

    add logging

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/GlobalExceptionHandler.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ch.noseryoung.REST_Foods.domain;
 
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,23 +13,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.UUID;
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException e) {
+        log.info("Handled exception occured", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(EntityExistsException.class)
     public ResponseEntity<String> handleEntityExists(EntityExistsException e) {
+        log.info("Handled exception occured", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleNotValidBody(MethodArgumentNotValidException e) {
+        log.info("Handled exception occured", e);
         String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
@@ -36,6 +41,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<String> handleWrongPathVariable(MethodArgumentTypeMismatchException e) {
+        log.info("Handled exception occured", e);
         if (e.getRequiredType() == UUID.class) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid UUID format!");
         }
@@ -45,20 +51,21 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArguments(IllegalArgumentException e) {
+        log.info("Handled exception occured", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleNoBody(HttpMessageNotReadableException e) {
+        log.info("Handled exception occured", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request body is unreadable, is missing or malformed. Please check your JSON structure and data types.");
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        //todo use slf4j
-        e.printStackTrace();
+        log.error("Unexpected exception occured", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Internal Server Error!");
     }
 }
