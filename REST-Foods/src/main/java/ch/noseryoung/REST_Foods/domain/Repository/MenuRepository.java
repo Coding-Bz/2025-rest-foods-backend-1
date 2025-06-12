8f3be69171749ec3885713afa823cc35b89e8ad2
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-06-12 14:09:06 +0200

    24-RepoUpdate
    Both repositories were updated because of the new Admin view,these changes are only going to affect the Admin files, all the other files stay untouched.

--- REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Repository/MenuRepository.java
+++ REST-Foods/src/main/java/ch/noseryoung/REST_Foods/domain/Repository/MenuRepository.java
@@ -1,10 +1,38 @@
 package ch.noseryoung.REST_Foods.domain.Repository;
+
 import ch.noseryoung.REST_Foods.domain.Model.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface MenuRepository   extends JpaRepository<Menu, UUID> {
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
+    long countByDietaryRequirements(String dietaryRequirements);
+
+    long countByCategory(String category);
+
+    long countByAllergies(String allergies);
+
+    long countByChefsChoice(boolean chefsChoice);
+
+    long countByPriceIsLessThan(double price);
+
+    long countByPriceIsGreaterThan(double price);
+
+
+    List<Menu> findByCategory(String category);
+
+    List<Menu> findByAllergies(String allergies);
+
+    List<Menu> findByDietaryRequirements(String dietaryRequirements);
+
+    List<Menu> findByChefsChoice(boolean chefsChoice);
+
+    List<Menu> findByPriceIsLessThan(double price);
+
+    List<Menu> findByPriceIsGreaterThan(double price);
+
 
 }
